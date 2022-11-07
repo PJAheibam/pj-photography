4a4fc8274c1c22d85784c01053587bc439c59c69
PJAheibam <prosenjitsingha28@gmail.com>
2022-11-07 22:35:57 +0600

    primary color set

---
+++ src/styles/theme/index.js
@@ -0,0 +1,25 @@
+export const darkTheme = {
+  palette: {
+    background: {
+      default: "hsl(270,10%,15%)",
+      paper: {
+        0: "transparent",
+        1: "hsl(270,15%,15%)",
+        2: "hsl(270,18%,15%)",
+        3: "hsl(270,11%,15%)",
+        4: "hsl(270,12%,15%)",
+        5: "hsl(270,14%,15%)",
+      },
+    },
+    primary: {
+      main: {
+        bg: "hsl(145,62%,35%)",
+        text: "hsl(145,100%,95%)",
+      },
+      container: {
+        bg: "hsl(145,45%,55%)",
+        text: "hsl(145,100%,5%)",
+      },
+    },
+  },
+};
