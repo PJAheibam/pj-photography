adb31c80b207bb17a04220549f8aee4aa0568fdb
PJAheibam <prosenjitsingha28@gmail.com>
2022-11-10 21:30:00 +0600

    nav menu bug fixed

--- src/layouts/navbar/nav-menu/index.jsx
+++ src/layouts/navbar/nav-menu/index.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import { Item, Items } from "./styles";
 import { navLinks } from "../data";
+import { useAuthContext } from "../../../context/auth-contex";
 
 function NavMenu({ direction = "horizontal" }) {
+  const { user, loading } = useAuthContext();
+  if (loading) return null;
   return (
     <Items direction={direction}>
-      {navLinks.map((link, i) => (
-        <Item key={i} to={link.to}>
-          {" "}
-          {link.name}{" "}
-        </Item>
-      ))}
+      {navLinks.map((link, i) => {
+        if (user && user?.uid) {
+          return (
+            <Item key={i} to={link.to}>
+              {" "}
+              {link.name}{" "}
+            </Item>
+          );
+        }
+        if (link.to !== "/add-service" && link.to !== "/my-reviews") {
+          return (
+            <Item key={i} to={link.to}>
+              {" "}
+              {link.name}{" "}
+            </Item>
+          );
+        }
+      })}
     </Items>
   );
 }
