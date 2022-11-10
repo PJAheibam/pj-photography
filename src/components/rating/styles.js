22f5297a2fb85fa9e6cf69f377aafb6d88cee1f8
PJAheibam <prosenjitsingha28@gmail.com>
2022-11-10 15:21:38 +0600

    rating style added

--- src/components/rating/styles.js
+++ src/components/rating/styles.js
@@ -3,9 +3,16 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   font-size: 1.15rem;
-  gap: 0.5em;
+  gap: 0.25em;
 `;
 export const Star = styled.button`
   font-size: inherit;
   color: hsl(50, 90%, 50%);
+  &:hover {
+    filter: drop-shadow(0 0 5px hsla(50, 90%, 50%, 0.5));
+    scale: 1.05;
+  }
+  &:active {
+    scale: 0.95;
+  }
 `;
