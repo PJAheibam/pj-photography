0a702775a620568fd8acbac40d13d79dc95e2c1c
PJAheibam <prosenjitsingha28@gmail.com>
2022-11-11 23:37:08 +0600

    some home page alignment fixed

--- src/pages/home/portfolio/index.jsx
+++ src/pages/home/portfolio/index.jsx
@@ -14,8 +14,13 @@ import {
 function PortfolioSection() {
   return (
     <Container>
-      <Heading>Portfolio</Heading>
-      <SubHeading>Here is some of my work</SubHeading>
+      <Heading>Wedding Photoshoot</Heading>
+      <SubHeading>
+        I have complete wedding photoshoot packages from pre-wedding photography
+        to holud wedding photography, couple photography, bridal photoshoot, and
+        you name it. Explore how we capture your holy bond of love and make the
+        special day of your life even more special.
+      </SubHeading>
       <Section>
         <ImageContainer>
           <Image
@@ -34,8 +39,8 @@ function PortfolioSection() {
           <Image src="https://solene.qodeinteractive.com/wp-content/uploads/2019/12/h7-port-feautured-img-2.jpg" />
         </ImageContainer>
         <Content>
-          <Title>EUGENE & KATHLEEN</Title>
-          <Text>Wedding Stories</Text>
+          <Title style={{ textAlign: "right" }}>EUGENE & KATHLEEN</Title>
+          <Text style={{ textAlign: "right" }}>Wedding Stories</Text>
         </Content>
       </Section>
       <Section>
--- src/pages/home/portfolio/styles.js
+++ src/pages/home/portfolio/styles.js
@@ -17,17 +17,6 @@ export const SubHeading = styled.p`
   font-size: 1.25rem;
 `;
 
-export const Section = styled.div`
-  margin-top: 2rem;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  @media ${device.sm} {
-    align-items: center;
-    flex-direction: ${(p) => (p.reverse ? "row-reverse" : "row")};
-  }
-`;
-
 export const ImageContainer = styled.figure`
   flex-basis: 100%;
   width: 50%;
@@ -43,15 +32,27 @@ export const Image = styled.img`
   object-position: center;
 `;
 
+export const Title = styled.h1`
+  /* text-align: center; */
+  font-size: clamp(1.5rem, 5vw, 2rem);
+`;
+export const Text = styled.p`
+  /* text-align: center; */
+  font-size: clamp(1.25rem, 5vw, 1.5rem);
+`;
+
 export const Content = styled.div`
   flex-basis: 100%;
 `;
 
-export const Title = styled.h1`
-  text-align: center;
-  font-size: clamp(1.5rem, 5vw, 2rem);
-`;
-export const Text = styled.p`
-  text-align: center;
-  font-size: clamp(1.25rem, 5vw, 1.5rem);
+export const Section = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  @media ${device.sm} {
+    align-items: center;
+    flex-direction: ${(p) => (p.reverse ? "row-reverse" : "row")};
+    /* justify-content: flex-end; */
+  }
 `;
